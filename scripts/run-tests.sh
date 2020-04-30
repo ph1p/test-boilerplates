1ab772ddef47d711f235a47db5fd6fcdf7ac46e8
Phil <phil@capshake.com>
2020-04-30 11:01:06 +0200

    build(travis): add npm ci and remove cache

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -3,21 +3,21 @@
 test_root=$PWD
 
 cd $test_root/ava-example
-npm i
+npm ci
 npm test
 npm run test:coverage
 
 cd $test_root/jasmine-example
-npm i
+npm ci
 npm test
 npm run test:coverage
 
 cd $test_root/jest-example
-npm i
+npm ci
 npm test
 npm run test:coverage
 
 cd $test_root/mocha-example
-npm i
+npm ci
 npm test
 npm run test:coverage
